9e9bdd3935f4beec4a86c22d3a593184408972ea
Paolo Chang <pchang@architech.ca>
2022-07-12 16:45:24 -0400

    Add ContextProvider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
+import { ContextProvider } from "./contexts/ContextProvider";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
