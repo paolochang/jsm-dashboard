b7f330868253dbda3c3271a803ef64a8261b38b9
Paolo Chang <pchang@architech.ca>
2022-07-18 11:26:21 -0400

    Add Customer page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ export { default as Ecommerce } from "./Ecommerce";
 export { default as Orders } from "./Orders";
 export { default as Employees } from "./Employees";
 // export { default as Editor } from "./Editor";
-// export { default as Customers } from "./Customers";
+export { default as Customers } from "./Customers";
 // export { default as ColorPicker } from "./ColorPicker";
 // export { default as Calendar } from "./Calendar";
 // export { default as Area } from "./Charts/Area";
